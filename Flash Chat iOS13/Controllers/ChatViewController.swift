fbeaa77d81529b1b0d1cbee5c81b9bdfe8a86e8b
Maksim Stogniy <mstogniy@ya.ru>
2024-04-06 21:26:47 +0300

    Add IQKeyboardManager

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,27 +7,40 @@
 //
 
 import UIKit
-import FirebaseAuth
+import Firebase
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
-    var messages: [Message] = [
-        Message(sender: "123@q.com", body: "Hello"),
-        Message(sender: "123@w.com", body: "Hi"),
-        Message(sender: "123@q.com", body: "What's up?")
-    ]
+    var messages: [Message] = []
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        loadMessages()
         tableView.dataSource = self
         navigationItem.hidesBackButton = true
         title = K.appName
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text,
+           let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
+            ]) { (error) in
+                if let e = error {
+                    print("Error is occured")
+                    return
+                }
+                
+                
+            }
+        }
     }
     
     @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
@@ -37,6 +50,33 @@ class ChatViewController: UIViewController {
             navigationController?.popToRootViewController(animated: true)
         } catch let signOutError as NSError {
           print("Error signing out: %@", signOutError)
+        }
+    }
+    
+    func loadMessages() {
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+                
+            self.messages = []
+            if error != nil {
+                print("Error is occured")
+                return
+            }
+            
+            if let snapshotDocuments = querySnapshot?.documents {
+                for document in snapshotDocuments {
+                    let data = document.data()
+                    if let sender = data[K.FStore.senderField] as? String,
+                       let body = data[K.FStore.bodyField] as? String {
+                        let newMessage = Message(sender: sender, body: body)
+                        self.messages.append(newMessage)
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                    }
+                }
+            }
         }
     }
     
